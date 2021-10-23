cce8dc32c7be9f501ee826186029b4efdf64c0fb
Mobile Apps <rafidtawhid@gmail.com>
2021-10-23 13:17:01 +0600

    data show into listtile from firebase.. done :D

--- lib/player_dashboard.dart
+++ lib/player_dashboard.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class PlayerDashboard extends StatefulWidget {
   const PlayerDashboard({Key? key}) : super(key: key);
 
@@ -9,65 +12,73 @@ class PlayerDashboard extends StatefulWidget {
 }
 
 class _PlayerDashboardState extends State<PlayerDashboard> {
+  final db = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('World Records'),centerTitle: true ,  leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),),
-        // body: Container(
-        //   decoration: BoxDecoration(
-        //     image: DecorationImage(
-        //       image: AssetImage(
-        //           "img/bg.jpg"),
-        //       fit: BoxFit.cover,
-        //     ),
-        //   ),
-        //   child: Column(
-        //     children: [
-        //       ListTile(
-        //         title: Text('Rafid'),
-        //         leading: RawMaterialButton(
-        //           onPressed: () {},
-        //           elevation: 18.0,
-        //
-        //           fillColor: Colors.white,
-        //           child: Icon(
-        //             Icons.wine_bar_rounded,
-        //             color: Colors.deepOrange,
-        //             size: 30.0,
-        //           ),
-        //           padding: EdgeInsets.all(15.0),
-        //           shape: CircleBorder(),
-        //         ),
-        //         subtitle: Row(
-        //           mainAxisSize: MainAxisSize.min,
-        //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //           children: [
-        //             Text('Legend',style: TextStyle(color: Colors.red),),
-        //             Text('11 jul 2021',style: TextStyle(color: Colors.blue),),
-        //           ],
-        //         ),
-        //         trailing: Text('522'),
-        //       ),
-        //
-        //     ],
-        //   ),
-        // ),
+        appBar: AppBar(
+          title: Text('World Records'),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
 
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('players').snapshots(),
-          builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot)
-          {
-            if(!snapshot.hasData)
-              {
-                return Center(child: Text("No Values"));
-              }
-            else
-              
+        body: StreamBuilder<QuerySnapshot>(
+          stream: db.collection('players').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else
+              return Container(
+
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("img/bg.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                  alignment: Alignment.topLeft,
+                child: ListView(
+                  children: snapshot.data!.docs.map((doc) {
+                    return ListTile(
+                      leading: RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 8.0,
+                        fillColor: Colors.white,
+                        child: Icon(
+                          Icons.wine_bar_rounded,
+                          color: Colors.deepOrange,
+                          size: 30.0,
+                        ),
+                        shape: CircleBorder(),
+                      ),
+                      title: Text(
+                        doc['name'],
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      subtitle: Row(
+
+                        children: [
+                          Text(
+                            doc['title'],style: TextStyle(color: Colors.red),
+
+                          ),
+                          Text(
+                            doc['date'],style: TextStyle(color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                      trailing: Text(doc['score'].toString()),
+                    );
+                  }).toList(),
+                ),
+              );
           },
         ),
       ),
